9da369c6a69611681b991b130233f5e9998f6abf
yilannelson <48736573+yilannelson@users.noreply.github.com>
2019-03-19 14:50:10 -0700

    Initial commit

---
+++ Vending_Machine.py
@@ -0,0 +1,104 @@
+def main_menu():
+    print("-----------------------------------\n"
+          "Main menu:\n"
+          "-----------------------------------\n"
+          "[1] Runny Nose\n"
+          "[2] Sore Throat\n"
+          "[3] Other\n"
+          "Select option 3 to exit>: ")
+
+def runny_nose():
+
+    while True:
+        print("-----------------------------------\n"
+          "Runny Nose menu:\n"
+          "-----------------------------------\n"
+          "[1] Water    \n"
+          "Juice    $0.99\n"
+          "Soda     $1.39\n"
+          "Select a drink by entering the full name <x to exit to the main menu>\n")
+
+        choice = input("Drink option: ")
+        choice = str(choice)
+        if choice == "1":
+            global balance
+            balance = float(balance) - 1
+            global total_purchase
+            if(balance >0):
+                print("Vending water, you have", str(balance), "dollars left")
+            else:
+                print("you don't have enough money to buy Water <", str(rdollar), "<", str(0.75), ">")
+
+        elif choice == "Juice":
+            balance = float(balance) - 0.99
+            if(balance >0):
+                print("Vending Juice, you have", str(balance), "dollars left")
+            else:
+                print("you don't have enough money to buy Juice <", str(rdollar), "<", str(0.99), ">")
+        elif choice == "Soda":
+            balance = float(balance) - 1.39
+            if(balance >0):
+                print("Vending Soda, you have", str(balance), "dollars left")
+            else:
+                print("you don't have enough money to buy Soda <", str(rdollar), "<", str(1.39), ">")
+        elif choice == "x":
+            return main_menu()
+        else:
+            print("invalid option!")
+
+def snacks_menu():
+
+    while True:
+        print("-----------------------------------\n"
+          "snacks menu:\n"
+          "-----------------------------------\n"
+          "Chips    $0.99\n"
+          "Peanuts  $0.5\n"
+          "Gum      $1.35\n"
+          "Select a snack by entering the full name <x to exit to the main menu>\n")
+
+        choice = input("Snack option: ")
+        choice = str(choice)
+        if choice == "Chips":
+            global balance
+            balance = float(balance) - 0.99
+            global total_purchase
+            if(balance >0):
+                print("Vending water, you have", str(balance), "dollars left")
+            else:
+                print("you don't have enough money to buy Chips <", str(rdollar), "<", str(0.99), ">")
+
+        elif choice == "Peanuts":
+            balance = float(balance) - 0.5
+            if(balance >0):
+                print("Vending Juice, you have", str(balance), "dollars left")
+            else:
+                print("you don't have enough money to buy Peanuts <", str(rdollar), "<", str(0.5), ">")
+        elif choice == "Gum":
+            balance = float(balance) - 0.35
+            if(balance >0):
+                print("Vending Gum, you have", str(balance), "dollars left")
+            else:
+                print("you don't have enough money to buy Gum <", str(rdollar), "<", str(0.35), ">")
+        elif choice == "x":
+            return main_menu()
+        else:
+            print("invalid option!")
+
+while True:
+
+    main_menu()
+    choice = input()
+    choice = int(choice)
+    if choice == 1:
+            balance = 1
+            drinks_menu()
+    elif choice == 2:
+            balance = 2
+            snacks_menu()
+    elif choice == 3:
+            print("-------------------------------\n"
+                  "Inserted amount: ", str(rdollar),",", "total purchase: ", rdollar-balance, ",", "change: ", balance)
+            exit()
+    else:
+            print("invalid option!")
